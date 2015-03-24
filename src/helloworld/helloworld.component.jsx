21af797623b1e9d65a4c3ba1d96b36bd3cc47f2d
Florent Lepretre <florent.lepretre@zenika.com>
2015-03-24 14:19:37 +0100

    TP0 Hello world !

---
+++ src/helloworld/helloworld.component.jsx
@@ -0,0 +1,10 @@
+'use strict';
+
+var React = require('react');
+
+var Helloworld = React.createClass({
+
+  // TODO define compoenent render function
+});
+
+module.exports = Helloworld;
---
+++ src/init.jsx
@@ -0,0 +1,6 @@
+'use strict';
+
+var React = require('react');
+var Hello = require('./helloworld/helloworld.component.jsx');
+
+// TODO Render the root element into the view
