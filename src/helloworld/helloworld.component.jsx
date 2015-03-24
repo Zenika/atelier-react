9307ec2bb1aaed408921dba0907f72409833bfc3
Florent Lepretre <florent.lepretre@zenika.com>
2015-03-24 14:13:24 +0100

    Solution TP0 Hello world !

---
+++ src/helloworld/helloworld.component.jsx
@@ -0,0 +1,17 @@
+'use strict';
+
+var React = require('react');
+
+var Helloworld = React.createClass({
+
+    // TODO define compoenent render function
+    render: function () {
+        return (
+            <div key="1">
+                <h2>Hello world !</h2>
+            </div>
+        );
+    }
+});
+
+module.exports = Helloworld;
