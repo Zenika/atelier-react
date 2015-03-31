ebcddcb49cd8388db1e776b7506e3490c3fcf19d
Florent Lepretre <florent.lepretre@zenika.com>
2015-03-31 22:54:12 +0200

    Solution TP2 Each component has its task

---
+++ src/beerLister/beerItem.component.jsx
@@ -0,0 +1,21 @@
+'use strict';
+
+var React = require('react');
+
+var BeerItem = React.createClass({
+  render: function () {
+    var beer = this.props.beer;
+
+    return (
+      <li className="list-group-item row">
+        <h3 className="list-group-item-heading">{beer.name}</h3>
+        <div className="list-group-item-text">
+          <p className="badge col-md-1">{beer.abv}%</p>
+          <p className="col-md-11">{beer.description}</p>
+        </div>
+      </li>
+    );
+  }
+});
+
+module.exports = BeerItem;
