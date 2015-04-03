d6240184fcc2a48813491944e4d4d2ed531458f8
Matthieu Lux <matthieu.lux@gmail.com>
2015-04-03 14:53:46 +0200

    babel, eslint, browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var gulp = require('gulp');
-var server = require('gulp-express');
-var webpack = require('gulp-webpack');
+var browserSync = require('browser-sync');
+
+var $ = require('gulp-load-plugins')();
 
 gulp.task('webpack', function () {
     gulp.src('src/init.jsx')
@@ -19,13 +20,55 @@ gulp.task('webpack', function () {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('serve', ['webpack'], function () {
-    server.run({
-        file: 'app.js'
-    });
+function webpack(watch, callback) {
+  return gulp.src('src/init.jsx')
+    .pipe($.webpack({
+      watch: watch,
+      module: {
+        preLoaders: [
+          { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'eslint-loader'}
+        ],
+        loaders: [
+          { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader'}
+        ]//,
+        //noParse: [/lie\.js/, /leveldown/, /levelup/]
+      },
+      output: {
+        filename: 'bundle.js'
+      }
+    }, null, function(err, stats) {
+      if(err) {
+        throw new $.util.PluginError('webpack', err);
+      }
+      $.util.log(stats.toString({
+        colors: $.util.colors.supportsColor,
+        chunks: false,
+        hash: false,
+        version: false
+      }));
+      browserSync.reload();
+      if(watch) {
+        watch = false;
+        callback();
+      }
+    }))
+    .pipe(gulp.dest('dist'));
+}
 
-    gulp.watch(['src/**/*.{html,css,js,jsx}'], ['webpack']);
-    gulp.watch(['src/**/*.{html,css,js,jsx}'], function (event) {
-        server.notify(event);
-    });
+gulp.task('webpack', function () {
+  return webpack(false);
+});
+
+gulp.task('webpack:watch', function (callback) {
+  return webpack(true, callback);
+});
+
+gulp.task('serve', ['webpack:watch'], function () {
+  browserSync.init({
+    startPath: '/',
+    server: {
+      baseDir: ['./dist', './src', './node_modules'],
+      middleware: require('./obd-proxy')
+    }
+  });
 });
