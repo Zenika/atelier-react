6e918979dacf6b2c6301d7c55e9fd0d342dd33c3
Florent Lepretre <florent.lepretre@zenika.com>
2015-04-08 22:59:42 +0200

    [WIP] Solution TP5 React-router

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,7 +6,7 @@ var Route = ReactRouter.Route;
 var DefaultRoute = ReactRouter.DefaultRoute;
 var Main = require('./beerLister/main.container.jsx');
 var BeersList = require('./beerLister/components/beerList.component.jsx');
-var BeerDetail = require('./beerLister/components/beerDetail.component.jsx');
+var BeerDetail = require('./beerLister/components/beerDetail.jsx');
 
 module.exports = (
   <Route name="main" handler={Main} path="/">
