1cc664c245df39a9bdb096a587dd012604f71bcf
Nicolas Cuillery <nicolas.cuillery@gmail.com>
2015-04-15 11:12:47 +0200

    Upload slides on github pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# atelier-react
-Atelier React Mix IT
--- src/init.jsx
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var React = require('react');
-var Hello = require('./helloworld/helloworld.component.jsx');
-
-// TODO Render the root element into the view
